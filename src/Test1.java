437ad5d8b118d56c888cd6a73b05124d12c09cff
sidrao02 <66804331+sidrao02@users.noreply.github.com>
2020-12-13 12:51:25 -0500

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -1,37 +1,24 @@
-import java.util.*;
-
-enum Operation
-{
-    ADD, SUBTRACT, MULTIPLY, DIVIDE
-}
-public class Test1 {
-
-    public static double basicCalculator(final Operation operation, final int num1, final int num2){
+public static double basicCalculator(Operation operation, int num1, int num2){
         double result = 0;
 
         switch(operation) {
             case ADD:
-                // if operation = ADD, return the sum
-                result = num1 + num2;
+                System.out.println("CS193 sucks");
                 break;
             case SUBTRACT:
-                result = num1 - num2;
+                System.out.println("CS193 is not everyone's favorite class");
                 break;
             case MULTIPLY:
-                result = num1 * num2;
+                System.out.println("The CS193 lecturers do not want us to thrive in CS");
                 break;
             case DIVIDE:
                 if (num2 == 0) {
-                    System.out.println("Can't divide by 0");
+                    System.out.println("Error: u suck");
                 } else {
-                    result = ((double)num1) / num2;
+                    result = num1 / num2;
                 }
                 break;
         }
+
         return result;
     }
-
-    public static void main(final String[] args) {
-        System.out.println(basicCalculator(Operation.DIVIDE, 1, 2));
-    }
-}
