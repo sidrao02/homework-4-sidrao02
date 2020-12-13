9c6d0c66252ee611af7af278f019531eb99516bf
Siddhartha Rao <rao138@purdue.edu>
2020-12-13 13:05:56 -0500

    if else test

--- src/Test1.java
+++ src/Test1.java
@@ -8,26 +8,24 @@ public class Test1 {
 
     public static double basicCalculator(final Operation operation, final int num1, final int num2){
         double result = 0;
+	if (operation == ADD){
+		result = num1 + num2;
+	}
+	else if (operation == SUBTRACT){
+		result = num1 - num3;
+	}
+	else if (operation = MULTIPLY){
+		result = num1 * num2;
+	}
+	else{
+		if (num2 == 0) {
+			System.out.println("Can't divide by 0");
+		}
+		else {
+			result = num1 / num2;
+		}
+	}
 
-        switch(operation) {
-            case ADD:
-                // if operation = ADD, return the sum
-                result = num1 + num2;
-                break;
-            case SUBTRACT:
-                result = num1 - num2;
-                break;
-            case MULTIPLY:
-                result = num1 * num2;
-                break;
-            case DIVIDE:
-                if (num2 == 0) {
-                    System.out.println("Can't divide by 0");
-                } else {
-                    result = ((double)num1) / num2;
-                }
-                break;
-        }
         return result;
     }
 
